5a8b7fcef5fe2150c31c85e7ac68779917a36042
BYDOC <burakyilmaz01@gmail.com>
2019-04-23 01:12:27 +0300

    insert/update customer
    insert/update customer

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -24,24 +25,71 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
 
+            //dogrudan view içinde gondermiyoruz. çünkü ileride custom context gondermek istediğimizde sorun olacak. viewmodel olusturup gonderiyoruz.
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipType = membershipTypes
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            //model binding
+            if (customer.Id==0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthday = customer.Birthday;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewletter= customer.IsSubscribedToNewletter;
+            }
+          
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
 
         // GET: Customers
-        public ViewResult Index() 
+        public ViewResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
         }
         public ActionResult Details(int id)
         {
-            var movie = _context.Customers.Include(c=>c.MembershipType).SingleOrDefault(c => c.Id == id);
+            var movie = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
             if (movie == null)
                 return HttpNotFound();
             return View(movie);
         }
 
-        
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
 
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipType = _context.MembershipTypes.ToList()
+
+            };
+            return View("CustomerForm", viewModel);
+        }
 
     }
+    
 }
---
+++ Vidly/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        //List yada Ienumerable kullanılabilir. ancak Listin sağladığı avantajlara ihtiyac yok add index vs.
+
+        public IEnumerable<MembershipType> MembershipType { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
